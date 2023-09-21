9ecdbe1407e82d113e8365ddf6107bb6a6513287
SK0RPIA <contact@skorpia.fr>
2023-09-21 16:09:20 +0200

    feat: :sparkles: Get Disk data info modify for RAID

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,10 +6,5 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public function setHeader(){
-          // Envoi de l'en-tête Content-Type pour spécifier que la réponse est une chaîne JSON
-          header('Content-Type: application/json');
-          header('Access-Control-Allow-Origin: *');
-  
-    }
+
 }
--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -9,7 +9,6 @@ class ServerController extends Controller
 
     public function initPanel()
     {
-        $this->setHeader();
         $output = shell_exec("cat /proc/cpuinfo | grep 'model name' | head -n 1 | awk -F ': ' '{print $2}'");
         $output2 = shell_exec("nproc --all");
         $cpuName = trim($output);
@@ -25,7 +24,6 @@ class ServerController extends Controller
 
     public function getCpuPercentage()
     {
-        $this->setHeader();
         $output = shell_exec("top -bn 1 | awk '/Cpu\(s\):/ {print $2}'");
         $totalUsage = floatval($output);
 
@@ -39,7 +37,6 @@ class ServerController extends Controller
 
     public function getRamData()
     {
-        $this->setHeader();
 
         $output = shell_exec('free');
         $lines = explode("\n", $output);
@@ -59,7 +56,6 @@ class ServerController extends Controller
 
     public function getRaidStatus()
     {
-        $this->setHeader();
 
         // Exécution de la commande 'mdadm' pour obtenir le statut des raids
         $output = shell_exec('mdadm --detail /dev/md0'); // Remplacez /dev/md0 par le nom correct de votre RAID
@@ -86,63 +82,34 @@ class ServerController extends Controller
         return response()->json($raidData);
     }
 
-    public function getDiskData()
+    public function getDiskData($targetDisks = [])
     {
-        $this->setHeader();
-
-        // Exécution de la commande 'lsblk' pour obtenir la liste des disques et partitions
-        $lsblkOutput = shell_exec('lsblk --output NAME,TYPE,MOUNTPOINT');
-        $lines = explode("\n", trim($lsblkOutput));
-
-        $diskData = [];
         $partitions = []; // Tableau pour stocker les partitions
 
-        foreach ($lines as $line) {
-            if (preg_match('/^(\S+)\s+(\S+)\s*(\S*)/', $line, $matches)) {
-                $cleanedName = preg_replace('/[^a-zA-Z0-9]/', '', $matches[1]); // Nettoyer le nom
 
+
+        $dfOutput = shell_exec('df /dev/md0');
+        $dfLines = explode("\n", trim($dfOutput));
+
+        if (count($dfLines) > 1) {
+            $diskLine = $dfLines[1]; // Prendre la seconde ligne
+            if (preg_match('/\/dev\/\S+\s+(\d+)\s+(\d+)\s+\d+\s+(\d+)%/', $diskLine, $dfMatches)) {
                 $diskInfo = [
-                    'name' => $cleanedName,
-                    'type' => $matches[2],
-                    'mount_point' => $matches[3] ?? null,
+                    'name' => "md0",
+                    'storage_total' => intval($dfMatches[1]),
+                    // Convertir les blocs de 1K en octets
+                    'storage_used' => intval($dfMatches[2]),
+                    // Convertir les blocs de 1K en octets
+                    'storage_usage_percentage' => $dfMatches[3] . '%',
                 ];
 
-                if ($diskInfo['name'] !== "NAME") {
-                    $dfOutput = shell_exec('df /dev/' . $diskInfo['name']);
-
-                    $dfLines = explode("\n", trim($dfOutput));
-
-                    if (count($dfLines) > 1) {
-                        $diskLine = $dfLines[1]; // Prendre la seconde ligne
-                        if (preg_match('/\/dev\/\S+\s+(\d+)\s+(\d+)\s+\d+\s+(\d+)%/', $diskLine, $dfMatches)) {
-                            $diskInfo['storage_total'] = intval($dfMatches[1]) * 1024; // Convertir les blocs de 1K en octets
-                            $diskInfo['storage_used'] = intval($dfMatches[2]) * 1024; // Convertir les blocs de 1K en octets
-                            $diskInfo['storage_usage_percentage'] = $dfMatches[3] . '%';
-                        }
-                    }
-
-                    // Si c'est un disque, ajoutez-le à la liste principale
-                    if ($diskInfo['type'] === 'disk') {
-                        $diskInfo['partitions'] = []; // Initialiser les partitions
-                        $diskData[$diskInfo['name']] = $diskInfo;
-                    }
-                    // Si c'est une partition, trouvez le disque parent et ajoutez-la à ses partitions
-                    elseif (isset($diskData[substr($diskInfo['name'], 0, 3)])) {
-                        $diskData[substr($diskInfo['name'], 0, 3)]['partitions'][] = $diskInfo;
-                        $partitions[] = $diskInfo; // Ajouter la partition à la liste des partitions
-                    }
-                }
+                ; // Ajouter la partition à la liste des partitions
             }
         }
+        array_push($partitions, $diskInfo);
 
         // Envoi de la chaîne JSON en tant que réponse
         return response()->json($partitions); // Renvoyer uniquement le tableau des partitions
     }
-
-
-
-
-
-
 
 }
